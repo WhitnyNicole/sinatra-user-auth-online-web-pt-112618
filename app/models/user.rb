434162b6605450d1ed8256fc2fbee3bc9b2c1f41
Whitny Edwards <wne4e2@gmail.com>
2019-03-23 01:11:53 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
   
-  def change 
-    create_table :user do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-    end 
-  end 
 end
